36b47f12cb510102348e0eb99f504fad8a9df40e
BD4vid777 <dawid.budzinski@apps-garden.com>
2023-05-05 12:36:57 +0200

    Updating Home screen and bg settings

--- src/app/utils/internal-data.ts
+++ src/app/utils/internal-data.ts
@@ -1,27 +1,28 @@
 export const primaryBG = {
   photoLink: '../assets/bgImg/christian-lue-unsplash.jpg',
-  photoAuthor: 'Photo by <a href="https://unsplash.com/@christianlue?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Christian Lue</a> on <a href="https://unsplash.com/backgrounds/phone/keyboard?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>\n'
+  photoAuthor: 'Photo by <a href="https://unsplash.com/@christianlue?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Christian Lue</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a>\n',
+  photoIndex: 0
 }
 
 export const backgroundImages = [
   {
     photoLink: '../assets/bgImg/daniel-sessler-unsplash.jpg',
-    photoAuthor: 'Photo by <a href="https://unsplash.com/@danielsessler?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Daniel Seßler</a> on <a href="https://unsplash.com/photos/mhGXpkMsdAU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>\n',
-    photoIndex: 0
-  },
-  {
-    photoLink: '../assets/bgImg/zetong-li-unsplash.jpg',
-    photoAuthor: 'Photo by <a href="https://unsplash.com/@zetong?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Zetong Li</a> on <a href="https://unsplash.com/photos/9G8K_nycv7s?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>\n',
+    photoAuthor: 'Photo by <a href="https://unsplash.com/@danielsessler?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Daniel Seßler</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a>\n',
     photoIndex: 1
   },
   {
-    photoLink: '../assets/bgImg/pramod-tiwari-unsplash.jpg',
-    photoAuthor: 'Photo by <a href="https://unsplash.com/@pramodtiwari?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Pramod Tiwari</a> on <a href="https://unsplash.com/photos/VlgYg9QZd0E?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>\n',
+    photoLink: '../assets/bgImg/zetong-li-unsplash.jpg',
+    photoAuthor: 'Photo by <a href="https://unsplash.com/@zetong?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Zetong Li</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a>\n',
     photoIndex: 2
   },
   {
-    photoLink: '../assets/bgImg/jason-blackeye-unsplash.jpg',
-    photoAuthor: 'Photo by <a href="https://unsplash.com/@jeisblack?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Blackeye</a> on <a href="https://unsplash.com/photos/byqgHC0XFME?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>\n',
+    photoLink: '../assets/bgImg/pramod-tiwari-unsplash.jpg',
+    photoAuthor: 'Photo by <a href="https://unsplash.com/@pramodtiwari?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Pramod Tiwari</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a>\n',
     photoIndex: 3
+  },
+  {
+    photoLink: '../assets/bgImg/jason-blackeye-unsplash.jpg',
+    photoAuthor: 'Photo by <a href="https://unsplash.com/@jeisblack?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Jason Blackeye</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a>\n',
+    photoIndex: 4
   }
-  ]
+]
